b3f0dc8f8bd25541d04e2cffcfc55e2e616013c3
abdlhaklalouche <abdlhaklalouche@gmail.com>
2019-07-03 11:03:02 +0100

    Update Enqueue.php and .gitignore

--- app/Providers/Enqueue.php
+++ app/Providers/Enqueue.php
@@ -3,7 +3,20 @@
 namespace Timpress\Providers;
 
 class Enqueue extends Provider {
-  public function register()
-  {
-  }
+  public function register() 
+	{
+		add_action('wp_enqueue_scripts',[$this, 'enqueue_scripts' ]);
+	}
+	/**
+   * Inject scripts inside html files.
+   * 
+   * @return void
+	 */
+	public function enqueue_scripts() 
+	{
+		// Stylesheet
+		wp_enqueue_style( 'main', mix('css/app.css'), [], '1.0.0', 'all' );
+		// Javascript
+		wp_enqueue_script( 'main', mix('js/app.js'), [], '1.0.0', true );
+	}
 }
